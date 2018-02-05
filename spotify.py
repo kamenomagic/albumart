8884118bdd6fad92a1a46e972a3d09d0fa8001a3
S. Jacob Powell <s.jacob.powell@gmail.com>
2018-02-05 10:08:11 -0700

    Updated requirements.txt, added really awesome printouts

--- spotify.py
+++ spotify.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python
 import os
 import sys
+from tqdm import tqdm
+from tqdm import trange
 
 import spotipy
 from pymongo import MongoClient
@@ -11,7 +13,7 @@ from config import *
 location_file_name = 'albart_current_scrape_location.txt'
 start_year = 2000
 # 2018
-end_year = 2003
+end_year = 2001
 # 100000?
 album_count_per_year = 3
 # 50
@@ -40,28 +42,35 @@ def main():
         exit()
     with open(location_file_name, 'r+' if location_exists else 'w+') as location_file:
         year, current_offset = init_location(location_file, location_exists)
-        for i in range(year, end_year, 1):
-            for j in range(current_offset, album_count_per_year, album_count_per_request):
+        year_iterator = trange(year, end_year, 1, leave=True)
+        for i in year_iterator:
+            year_iterator.set_description('Year:   {:>100}'.format(i))
+            album_iterator = trange(current_offset, album_count_per_year, album_count_per_request, leave=True)
+            for j in album_iterator:
+                album_iterator.set_description('Offset: {:>100}'.format(j))
                 current_offset = 0
                 finished = scrape_year(i, j)
                 save_location(i, j, location_file)
-                print('Finished and saved location: {}-{}'.format(i, j))
+                tqdm.write('Finished and saved location: {}-{}'.format(i, j))
                 if finished:
                     break
-        print('Completely finished scraping, so deleting location file.')
+        tqdm.write('\n\n\nCompletely finished scraping, so deleting location file.')
         os.remove(location_file_name)
 
 
 def scrape_year(year, offset):
     # Returns true when it has scraped all albums
     albums = get_albums(year, offset=offset, limit=album_count_per_request)
-    for album in albums:
-        for track in spotify.album_tracks(album['id'])['items']:
+    album_iterator = tqdm(albums)
+    for album in album_iterator:
+        album_iterator.set_description('Album:  {:>100}'.format(album['name']))
+        track_iterator = tqdm(spotify.album_tracks(album['id'])['items'])
+        for track in track_iterator:
+            track_iterator.set_description('Track:  {:>100}'.format(track['name']))
             track['analysis'] = spotify.audio_analysis(track['id'])
             track['analysis']['available_markets'] = None
             track['features'] = spotify.audio_features([track['id']])
             tracks.insert_one(track)
-            break
     return len(albums) < album_count_per_request
 
 
@@ -78,7 +87,7 @@ def init_location(location_file, location_exists):
     current_offset = int(location_strings[1]) if location_exists else 0
     save_location(year, current_offset, location_file)
     if not location_exists:
-        print('Created location file, starting from location {}-{}'.format(year, current_offset))
+        tqdm.write('Created location file; starting to scrape from location {}-{}'.format(year, current_offset))
     return year, current_offset
 
 
