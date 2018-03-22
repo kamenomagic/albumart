9b90db73587bcf44716e1e4776ffebd6220735c3
Samuel G-C <samuel.jingc@gmail.com>
2018-03-22 09:15:07 -0600

    added color selection to example read img

--- example_read_img.py
+++ example_read_img.py
@@ -11,15 +11,23 @@ def main():
     db = mongo.albart
     albums = db.albums
 
-    srch_dict = {'artist': 'AC/DC'}
-    test = albums.find(srch_dict)
-    for album in test:
+    srch_dict = {}
+    search = albums.find(srch_dict)
+    dataset = []
+    color = [0, 0, 255]
+    for album in search:
         img = cStringIO.StringIO(base64.b64decode(album["image"]))
         img = np.asarray(Image.open(img))
+        test = np.mean(img, axis=(0, 1))
+        if np.sum(np.absolute(test - color)) < 260:
+            dataset.append(img)
+    print(len(dataset))
+    """
+    for entry in dataset:
         plt.figure(1)
-        plt.imshow(img)
+        plt.imshow(entry)
         plt.show()
-
+    """
 
 if __name__ == '__main__':
     main()
