135945e652762e2e4683fb8f887d8d609594998d
S. Jacob Powell <s.jacob.powell@gmail.com>
2018-03-27 06:17:45 -0600

    Added basic noun extraction with lemmatization, ordering by position in the document and frequency

---
+++ language.py
@@ -0,0 +1,41 @@
+#! /usr/bin/python
+import spacy
+from spacy.lemmatizer import Lemmatizer
+from spacy.lang.en import LEMMA_INDEX, LEMMA_EXC, LEMMA_RULES
+
+class Language:
+    def __init__(self):
+        self.nlp = spacy.load('en')
+        self.lemmatizer = Lemmatizer(LEMMA_INDEX, LEMMA_EXC, LEMMA_RULES)
+
+    def get_top_nouns(self, text):
+        doc = self.nlp(unicode(text, 'utf-8'))
+        ordered_keys = []
+        counts = {}
+        for token in doc:
+            if token.pos_ == 'NOUN':
+                lemmatized_noun = self.lemmatizer(token.text, token.pos_)[0]
+                if lemmatized_noun in counts:
+                    counts[lemmatized_noun] += 1
+                else:
+                    counts[lemmatized_noun] = 1
+                    ordered_keys.append(lemmatized_noun)
+        # sorted_by_value = [key for key, value in sorted(count.iteritems(), key=lambda (k,v): (v,k))]
+        result = []
+        for key in ordered_keys:
+            if len(result)  == 0:
+                result.append(key)
+            else:
+                inserted = False
+                for i, compare_key in enumerate(result):
+                    if counts[compare_key] < counts[key]:
+                        result.insert(key, i)
+                        inserted = True
+                if not inserted:
+                    result.append(key)
+        return [str(word) for word in result]
+
+
+
+if __name__ == '__main__':
+    print(Language().get_top_nouns('Hello friend, you are a cat and I am a bird, and cat\'s and birds are my best friends!'))
